7644c46ac75c0e83246d5f9189aec0dcf6431bdf
Ayesh <ayeshqumhieh@gmail.com>
2024-04-23 18:20:32 +0300

    Navigation component dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,13 +33,26 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    dataBinding {
+        enable = true
+    }
 }
 
 dependencies {
 
+    // androidx and appcompat
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+
+    // material
     implementation("com.google.android.material:material:1.11.0")
+
+    //navigation component
+    val navVersion = "2.7.7"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    //Test Dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
